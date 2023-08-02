c1227592838d251b32f37322be4d67e4aa048784
nainshisingh985@gmail.com <nainshisingh985@gmail.com>
2023-08-02 14:54:43 +0530

    Initial Commit

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StructsExample
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            doApp();
+            Console.ReadKey();
+        }
+
+        static void doApp()
+        {
+            // Create a student using the struct
+            Student student1;
+            student1.Name = "John";
+            student1.Age = 20;
+            student1.Major = "Computer Science";
+
+            // Print student details
+            Console.WriteLine("Student Name: " + student1.Name);
+            Console.WriteLine("Age: " + student1.Age);
+            Console.WriteLine("Major: " + student1.Major);
+        }
+    }
+
+ 
+    struct Student
+    {
+        public string Name;
+        public int Age;
+        public string Major;
+    }
+
+}
